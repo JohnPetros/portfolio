2e26689bdb8b1da4bb4564c697727bd9fb9e35d4
JohnPetros <joaopedro.nc@outlook.com>
2024-05-07 14:38:16 -0300

    ♻️ refactor: enable get any file py path using a global function

--- api/app.py
+++ api/app.py
@@ -4,14 +4,19 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 
-@app.template_global("get_image")
-def get_image(image_name):
-    return url_for("static", filename=f"images/{image_name}")
+@app.template_global("get_file")
+def get_file(file_path):
+    return url_for("static", filename=file_path)
 
 
 @app.route("/")
 def home():
     return render_template("pages/home/index.html")
+
+
+@app.route("/projects")
+def projects():
+    return render_template("pages/projects/index.html")
 
 
 if __name__ == "__main__":
