3828c7ba1e701c60be785f84ec835a7312a8ac9f
JohnPetros <joaopedro.nc@outlook.com>
2024-05-11 13:54:12 -0300

    💡 scripts: add header js

---
+++ api/static/scripts/header.js
@@ -0,0 +1,27 @@
+class Header {
+  activeClass = "bg-gray-900 rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-70 shadow-lg translate-y-0.5"
+
+  constructor() {
+    const container = document.querySelector('[data-header="container"]')
+    const trigger = document.querySelector('[data-header="trigger"]')
+
+    if (!container || !trigger) return
+
+    this.container = container
+    const intersectObserver = new IntersectionObserver(
+      (entries) => this.handleIntersectObserver(entries[0])
+    )
+
+    intersectObserver.observe(trigger)
+  }
+
+  handleIntersectObserver(trigger) {
+    if (trigger.isIntersecting || trigger.boundingClientRect.y < 0) {
+      this.container.classList.add(...this.activeClass.split(" "))
+    } else {
+      this.container.classList.remove(...this.activeClass.split(" "))
+    }
+  }
+}
+
+window.addEventListener("load", () => new Header())
