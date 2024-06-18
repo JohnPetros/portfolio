1ac0b060f5bc82daedb9841ffdbda3cf5da94272
JohnPetros <joaopedro.nc@outlook.com>
2024-06-17 22:23:56 -0300

    💡 scripts: add animated title js

---
+++ api/static/scripts/animated_title.js
@@ -0,0 +1,47 @@
+class AnimatedTitle {
+  constructor() {
+    const containers = document.querySelectorAll("[data-animated-title='container']")
+
+    this.containers = containers
+
+    this.addIntersectObserver()
+  }
+
+  addIntersectObserver() {
+    for (const container of this.containers) {
+      const intersectObserver = new IntersectionObserver(
+        (entries) => this.handleIntersectObserver(entries[0], container)
+      )
+
+      console.log({ container })
+
+      intersectObserver.observe(container)
+    }
+  }
+
+  handleIntersectObserver(state, container) {
+    if (state.isIntersecting)
+      this.animate(container)
+  }
+
+  animate(container) {
+    const fade = container.querySelector("[data-animated-title='fade']")
+
+    gsap.to(fade, {
+      keyframes: [
+        { opacity: 0, duration: .25 },
+        { opacity: .80, duration: .25 },
+      ],
+    })
+
+    gsap.to(container, {
+      keyframes: [
+        { x: container.offsetWidth + 48, duration: 2, delay: .5, ease: "ease-in-out" },
+        { opacity: 0, duration: .25, },
+      ],
+    });
+
+  }
+}
+
+window.addEventListener("load", () => new AnimatedTitle())
